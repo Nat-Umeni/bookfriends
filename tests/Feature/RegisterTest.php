1222f806d5ddf292899be7059ab967cbb6d3111b
Nat-Umeni <itsnat381@gmail.com>
2025-08-13 14:08:18 +0100

    Register tests done, need to do the login flow now

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,29 @@
+<?php
+
+// it('displays the register page')->get('/register')->assertSee('Register')->assertStatus(200);
+it('displays the register page', function () {
+    $this->get(route('register'))->assertStatus(200)->assertSeeText('Register');
+});
+
+it('throws validation errors for missing required attrs', function () {
+    $this->from(route('register'))
+        ->post(route('register.store'))
+        ->assertRedirect(route('register'))
+        ->assertSessionHasErrors(['name', 'email', 'password']);
+});
+
+
+it('successfully registers a new user with valid data and redirects to the home page', function () {
+    $this->from(route('register'))
+        ->post(route('register.store'), [
+            'name' => 'John Doe',
+            'email' => 'H2a0K@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ])->assertRedirect(route('home'));
+
+    $this->assertDatabaseHas('users', [
+        'name' => 'John Doe',
+        'email' => 'H2a0K@example.com',
+    ]);
+});

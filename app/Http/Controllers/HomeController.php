cd15cec4573ef417b10023e57348d5b95e54fe25
Nat-Umeni <itsnat381@gmail.com>
2025-08-14 14:43:54 +0100

    Removed debig logs, from trying to get crawler to work properly

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,23 +11,13 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $user = $request->user();
-
-        Log::debug('user', ['user' => $user]);
-
         $booksByStatus = $user
             ? $user
                 ->books()
                 ->get()
                 ->groupBy('pivot.status')
             : collect();
-
-
-        // $booksByStatus = $user
-        //     ? $user->books()->withPivot('status')->get()->groupBy('pivot.status')
-        //     : collect();
-
-        // dd('booksByStatus', (['keys' => $booksByStatus->keys()->all(), 'counts' => $booksByStatus->map->count()]));
-
+            
         // Build a dumb, renderable structure for the view
         $sections = collect(BookUser::rawAllowedStatuses())
             ->map(fn(string $label, string $key) => [
